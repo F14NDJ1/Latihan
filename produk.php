d34ccc60762e2bede6a5936297c97b21b6dfe707
F14NDJ1E <firmanuciha@gmail.com>
2021-09-13 16:37:35 +0800

    LATIHAN

--- produk.php
+++ produk.php
@@ -1,45 +1,12 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
 
+<h1>Nama : FIRMAN ANSHARI ADJIE</h1>
+<p>ALAMAT : MATARAM</p>
 
-class Produk
-{
-	public $judul = 'judul'
-	, $penulis = 'penulis'
-	, $penerbit = 'penerbit'
-	,$harga = 0;
-
-	function __construct($judul,$penulis,$penerbit,$harga)
-	{
-	     	$this->judul=$judul;
-		$this->penulis=$penulis;
-		$this->penerbit=$penerbit;
-		$this->harga=$harga;
-		echo "string<br>";
-
-	}
-
-	public function getLabel(){
-		return "$this->penulis,$this->penerbit"	;
-	}
-}
-
-
-$Produk1 = new Produk('Naruto','MasashiKisimoto','Bandai',30000);
-$Produk2 = new Produk('Spiderman','StanLee','Marvel',50000);
-var_dump($Produk1);
-echo "<br>";
-var_dump($Produk2);
-
-// echo "Komik : $Produk1->penulis,$Produk1->penerbit" ;
-// echo "<br>";
-
-// echo $Produk1->getLabel();
-// echo "<hr>";
-
-
-
-echo "new ni" . $Prodduk1->getLabel();
-
-echo "komik : " . $Produk1->getLabel();
-echo "<br>";
-echo "film : ". $Produk2->getLabel();
+</body>
+</html>
